5f96de329a1af738b2304a046353079f0bd6f8a2
Alex Sturtz <alexstur@gmail.com>
2020-10-12 17:51:18 -0500

    Add initial code for loading/preprocessing wikilarge dataset

---
+++ edit_nts/dataset.py
@@ -0,0 +1,43 @@
+import os
+
+WIKILARGE_PATH = '/common/cse896/asturtz/data-simplification/wikilarge'
+WIKISMALL_PATH = '/common/cse896/asturtz/data-simplification/wikismall'
+
+def load_raw_lines(path_prefix):
+    src_path = path_prefix + '.src'
+    dst_path = path_prefix + '.dst'
+    
+    src_lines = []
+    dst_lines = []
+
+    with open(src_path, 'r') as src_file:
+        src_lines = src_file.readlines()
+
+    with open(dst_path, 'r') as dst_file:
+        dst_lines = dst_file.readlines()
+
+    return src_lines, dst_lines
+
+def serialize_tokens(path_prefix, tokens):
+    token_path = path_prefix + '.pickle'
+
+    with open(token_path, 'wb') as token_file:
+        pickle.dump(tokens, token_file)
+    
+    return None
+
+def deserialize_tokens(path_prefix):
+    token_path = path_prefix + '.pickle'
+
+    tokens = None
+    with open(token_path, 'rb') as token_file:
+        tokens = pickle.load(token_file)
+
+    return tokens
+
+def wikilarge():
+    TRAIN_FILE_PREFIX = 'wiki.full.aner.ori.train'
+    train_path_prefix = os.path.join(WIKILARGE_PATH, TRAIN_FILE_PREFIX)
+    
+    return load_raw_lines(train_path_prefix)
+
---
+++ edit_nts/preprocess.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import pickle
+
+import spacy
+
+import dataset
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser', 'textcat'])
+
+# Adapted from https://github.com/yuedongP/EditNTS/blob/master/data_preprocess.py
+# Currently don't have a use for this
+def remove_lrb(sent_string):
+    frac_list = sent_string.split('-LRB-')
+    clean_list = []
+    for phrase in frac_list:
+        if '-rrb-' in phrase:
+            clean_list.append(phrase.split('-RRB-')[1].strip())
+        else:
+            clean_list.append(phrase.strip())
+    clean_sent_string =' '.join(clean_list)
+    return clean_sent_string
+
+# Currently just feeds the complex and simple sentences into spaCy.
+# There probably isn't a good reason to do this for the simplified text.
+def process_raw_lines(raw_comp_lines, raw_simp_lines):
+    comp_tokens = list(nlp.pipe(raw_comp_lines))
+    simp_tokens = list(nlp.pipe(raw_simp_lines))
+
+    return {
+        'comp_tokens': comp_tokens,
+        'simp_tokens': simp_tokens
+    }
+
+def main():
+    print('Loading wikilarge')
+    comp_lines, simp_lines = dataset.wikilarge()
+    
+    print('Processing raw data')
+    tokens = process_raw_lines(comp_lines, simp_lines)
+
+    print('Processed {} tokens'.format(len(tokens['comp_tokens'])))
+
+if __name__ == "__main__":
+    main()
